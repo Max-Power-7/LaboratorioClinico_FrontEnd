d6183a6a834ec5b2efade0046d4c8be3aa843eed
Jean Paul <lunafloresjeanpaul@hotmail.com>
2022-06-21 19:47:41 -0400

    Componentes Admin LTE separados de la carpeta components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,11 @@ import { ListarPacienteComponent} from './components/listar-paciente/listar-paci
 import { LaboratoristaComponent } from './components/laboratorista/laboratorista.component';
 import { ServiciosSaludComponent } from './components/servicios-salud/servicios-salud.component';
 
-import { ContentWrapperComponent } from './components/content-wrapper/content-wrapper.component';
-import { ControlSidebarComponent } from './components/control-sidebar/control-sidebar.component';
-import { MainFooterComponent } from './components/main-footer/main-footer.component';
-import { MainHeaderComponent} from './components/main-header/main-header.component';
-import { MainSidebarComponent } from './components/main-sidebar/main-sidebar.component';
+import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
+import { ControlSidebarComponent } from './control-sidebar/control-sidebar.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
+import { MainHeaderComponent} from './main-header/main-header.component';
+import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
 import { CommonModule } from '@angular/common';
 
 const routes: Routes = [
@@ -23,15 +23,15 @@ const routes: Routes = [
     {path: '**', redirectTo: '', pathMatch: 'full'},
     { path: 'laboratorista', component: LaboratoristaComponent },
     { path: 'servicios', component: ServiciosSaludComponent },
-   
-    
+
+
 ];
 
 @NgModule({
     declarations:[],
     imports: [CommonModule,RouterModule.forRoot(routes)],
     exports: [RouterModule],
-    
+
 })
 
 export class AppRoutingModule{}
