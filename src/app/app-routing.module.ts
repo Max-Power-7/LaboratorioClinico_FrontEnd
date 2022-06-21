b2c3337bf82c5adfe972578cd09704d0074d8136
Jean Paul <lunafloresjeanpaul@hotmail.com>
2022-06-21 13:16:28 -0400

    Componentes de laboratorista y servicios-salud creados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ListarOrdenComponent } from './components/listar-orden/listar-orden.component';
+import { LaboratoristaComponent } from './components/laboratorista/laboratorista.component';
+import { ServiciosSaludComponent } from './components/servicios-salud/servicios-salud.component';
 
 
 const routes: Routes = [
-  { path: 'orden', component: ListarOrdenComponent }
+  { path: 'laboratorista', component: LaboratoristaComponent },
+  { path: 'servicios', component: ServiciosSaludComponent },
+  { path: 'orden', component: ListarOrdenComponent },
 ];
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule,RouterModule.forRoot(routes)],
-  exports:[RouterModule]
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,15 @@ import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
 import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
 import { ControlSidebarComponent } from './control-sidebar/control-sidebar.component';
 import { MainFooterComponent } from './main-footer/main-footer.component';
+import { AppRoutingModule } from './app-routing.module';
+
+// Componentes de Leonel
 import { ListarOrdenComponent } from './components/listar-orden/listar-orden.component';
 import { CrearOrdenComponent } from './components/crear-orden/crear-orden.component';
-import { AppRoutingModule } from './app-routing.module';
+
+// Componentes de Jean Paul
+import { ServiciosSaludComponent } from './components/servicios-salud/servicios-salud.component';
+import { LaboratoristaComponent } from './components/laboratorista/laboratorista.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +26,9 @@ import { AppRoutingModule } from './app-routing.module';
     ControlSidebarComponent,
     MainFooterComponent,
     ListarOrdenComponent,
-    CrearOrdenComponent
+    CrearOrdenComponent,
+    ServiciosSaludComponent,
+    LaboratoristaComponent
   ],
   imports: [
     BrowserModule,
