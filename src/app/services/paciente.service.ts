6f0b1f73dee4f23ef64235ec14b0e46d652e4e71
luisbeltrancondori <luisbeltrancondoricano@gmail.com>
2022-06-21 18:49:41 -0400

    Consumiendo API

--- src/app/services/paciente.service.ts
+++ src/app/services/paciente.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Paciente } from '../models/paciente';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PacienteService {
 
-  constructor() { }
+  url = 'http://localhost:4000/api/paciente/';
+  
+  constructor(private http: HttpClient) { }
+
+  getPaciente(): Observable<any> {
+    return this.http.get(this.url);
+  }
+
+  eliminarPaciente(id: string): Observable<any> {
+    return this.http.delete(this.url + id);
+  }
+
+  guardarPaciente(paciente: Paciente):Observable<any> {
+    return this.http.post(this.url, paciente);
+
+  }
+
+
+  obtenerPaciente(id: string): Observable<any>{
+    return this.http.get(this.url + id);
+  }
+
+  editarPaciente(id: string, paciente: Paciente): Observable<any>{
+    return this.http.put(this.url + id, paciente);
+  }
+
 }
