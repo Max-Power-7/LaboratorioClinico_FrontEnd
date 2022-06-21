cc07dc649440bd9170e5b0f3ea2e4393a11a0758
Jean Paul <lunafloresjeanpaul@hotmail.com>
2022-06-21 08:42:53 -0400

    Diagrama de clases adjuntado en carpeta "archivos"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
